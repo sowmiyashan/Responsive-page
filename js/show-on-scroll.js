1cc9b7829990f73738f4cb3ebd3366b6729c1823
sowmiyashan2653@gmail.com <sowmiyashan2653@gmail.com>
2021-01-02 18:11:06 +0530

    Fourth Part done

--- js/show-on-scroll.js
+++ js/show-on-scroll.js
@@ -18,3 +18,23 @@ $(window).scroll( function(){
     }); 
 
 });
+
+$(window).scroll( function(){
+
+    /* Check the location of each desired element */
+    $('.last-s').each( function(i){
+
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        /* If the object is completely visible in the window, fade it in */
+        if( bottom_of_window > bottom_of_object ){
+
+            $('.hide-me-s').animate({'opacity':'1'},500).addClass("w3-animate-bottom")
+            $('.hide-me-sl').animate({'opacity':'1'},500).addClass("w3-animate-left")
+
+        }
+
+    }); 
+
+});
