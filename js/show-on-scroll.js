a07aa0f51c41cff4ce3d57c1d031607ee674ab78
sowmiyashan2653@gmail.com <sowmiyashan2653@gmail.com>
2021-01-02 21:21:39 +0530

    Fifth part done

--- js/show-on-scroll.js
+++ js/show-on-scroll.js
@@ -38,3 +38,44 @@ $(window).scroll( function(){
     }); 
 
 });
+
+$(window).scroll( function(){
+
+    /* Check the location of each desired element */
+    $('.last-1').each( function(i){
+
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        /* If the object is completely visible in the window, fade it in */
+        if( bottom_of_window > bottom_of_object ){
+
+            $('.last-1t').animate({'opacity':'1'},500).addClass("w3-animate-top")
+            $('.last-1b').animate({'opacity':'1'},500).addClass("w3-animate-bottom")
+
+        }
+
+    }); 
+
+});
+
+$(window).scroll( function(){
+
+    /* Check the location of each desired element */
+    $('.last-2').each( function(i){
+
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        /* If the object is completely visible in the window, fade it in */
+        if( bottom_of_window > bottom_of_object ){
+
+            $('.last-2t').animate({'opacity':'1'},500).addClass("w3-animate-top")
+            $('.last-2b').animate({'opacity':'1'},500).addClass("w3-animate-bottom")
+
+        }
+
+    }); 
+
+});
+
